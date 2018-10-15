510744014d22b21a52bcbca8bc3f8ab8ff6b344b
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-10-15 11:16:06 -0500

    errors

--- Final Project/Header.h
+++ Final Project/Header.h
@@ -1,4 +1,11 @@
 //header File
 //Authors: Evan J. Raker & Phero Luong
 //Date: 10/8/2018
-#pragma once
+#ifndef finalProject
+#define finalProject
+#include <iostream>
+
+int cardDraw();
+int goFish();
+
+#endif
