49a0b1fafd58eef7dcdb42c1bd557429fda4ae23
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-10-15 10:13:51 -0500

    Update2

--- Final Project/TicTacToe.cpp
+++ Final Project/TicTacToe.cpp
@@ -1,27 +1,11 @@
 #include <string>
 #include <iostream>
-#include <cstdio>
-#include <cstdlib>
+
 using namespace std;
 int ticTacToe()
 {
-	int computerRandomPick; 
-	int computerPick;
-	int playerChoice;
-	bool playerTurn;
-	int gameWin = 3;
-
-	char blockOne = '1';
-	char blockTwo = '2';
-	char blockThree = '3';
-	char blockFour = '4';
-	char blockFive = '5';
-	char blockSix = '6';
-	char blockSeven = '7';
-	char blockEight = '8';
-	char blockNine = '9';
-	int turnFirst; 
+	
 
 	system("pause");
-	return 0;
+	return 
 }
