7146bf5491b07c71c9956d767ece2f1c720a6265
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-10-17 14:52:38 -0500

    update

--- Final Project/TicTacToe.cpp
+++ Final Project/TicTacToe.cpp
@@ -2,31 +2,14 @@
 #include <iostream>
 #include <conio.h>
 #include <graphics.h>
-
+#include <iostream>
+#include <string>
 using namespace std;
 int ticTacToe()
 {
-	int maxX;
-	int maxY;
-	enum shape {X,O};
-	enum players { player1, player2 };
-	int board[9] = { -1 };
-	int gameOver = 0;
-	int turn = player1;
-	int currentMode = X;
-	int userBlockChoice;
-	int gameCounter = 0;
-
-	void plotBoard();
-	void plotShape(shape s, int block);
-
-	int board()
-	{
-		int graphicMaker = DETECT, graphicMode;
-	}
-
+	cout << "Welcome to tick tack Toe " << endl;
+	cout << "Player1[X]\ Player2[O]"
 	
-
 	system("pause");
-	return 
+	return 0;
 }
